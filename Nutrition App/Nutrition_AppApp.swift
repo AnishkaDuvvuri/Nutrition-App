600712884a5141dc1832fc41c85a2d5faab67f3f
Anishka <anishka.duvvuri@gmail.com>
2022-11-06 12:24:01 -0800

    Initial Commit

---
+++ Nutrition App/Nutrition_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_AppApp.swift
+//  Nutrition App
+//
+//  Created by Anishka Duvvuri on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NutritionPredict()
+        }
+    }
+}
